0176c58c4ff0d1cc22dc171792cb73c42a14ba6f
Raj Rao <raj.mobility@gmail.com>
2023-04-07 17:13:31 +0530

    Blog post with image

--- src/screens/PostsScreen.tsx
+++ src/screens/PostsScreen.tsx
@@ -1,19 +1,24 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, ActivityIndicator, FlatList} from 'react-native';
-import { Post } from '../components/DataTypes';
+import {View, Text, ActivityIndicator, FlatList, Image} from 'react-native';
+import {Post} from '../components/DataTypes';
 
 const PostsScreen = () => {
   const [isLoading, setLoading] = useState(true);
   const [posts, setPosts] = useState<Post[]>([]);
 
-  function randomUser(min:number, max:number) { // min and max included
-    return Math.floor(Math.random() * (max - min + 1) + min)
+  function randomUser(min: number, max: number) {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
   const getPostsData = async () => {
     try {
-      let userId:number = randomUser(1, 10);
-      const response = await fetch('https://jsonplaceholder.typicode.com/users/'+ userId.toString()+'/posts');
+      let userId: number = randomUser(1, 10);
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users/' +
+          userId.toString() +
+          '/posts',
+      );
       const json = await response.json();
       setPosts(json);
     } catch (error) {
@@ -23,31 +28,32 @@ const PostsScreen = () => {
     }
   };
 
- 
   useEffect(() => {
     getPostsData();
   }, []);
 
-  const renderPostDetails = (item: any) =>{
-    return <View>
-    <View style={{flexDirection: 'row'}}>
-        <View style={{flex: 1, paddingRight: 5}}>
-        <Text>
-          {item.item.title}
-        </Text>
+  const renderPostDetails = (item: any) => {
+    return (
+      <View>
+        <View style={{flexDirection: 'row'}}>
+          <View style={{flex: 1, paddingRight: 5, alignItems:'center', justifyContent:'center'}}>
+            <Image source={require('../../resources/blogpost.png')}   style={{width: 60, height: 60}}/>
+          </View>
+          <View style={{flex: 3}}>
+            <Text style={{fontWeight:'bold', marginBottom:5}}>{item.item.title}</Text>
+            <Text>{item.item.body}</Text>
+          </View>
         </View>
-        <View style={{flex: 3}}>
-        <Text>
-          {item.item.title}
-        </Text>
-        <Text>
-          {item.item.body}
-        </Text>
-        </View>
-        </View>
-        <View style={{borderWidth: 1, backgroundColor: '#CCCCCC', marginVertical: 20}} />
-        </View>
-  }
+        <View
+          style={{
+            borderWidth: 1,
+            backgroundColor: '#CCCCCC',
+            marginVertical: 20,
+          }}
+        />
+      </View>
+    );
+  };
 
   return (
     <View style={{flex: 1, padding: 24}}>
@@ -62,6 +68,6 @@ const PostsScreen = () => {
       )}
     </View>
   );
-  }
+};
 
 export default PostsScreen;
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,50 +1,71 @@
 import React, {useState, useEffect} from 'react';
-import {SafeAreaView,View, Text, ActivityIndicator} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  ActivityIndicator,
+  StyleSheet,
+} from 'react-native';
 import {User} from '../components/DataTypes';
 
 const ProfileScreen = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [users, setUsers] = useState<User[]>([]);
 
-    const [isLoading, setLoading] = useState(true);
-    const [users, setUsers] = useState<User[]>([]);
- 
-    console.log('Users', users);
-    function randomUser(min:number, max:number) { // min and max included
-      return Math.floor(Math.random() * (max - min + 1) + min)
+  console.log('Users', users);
+  function randomUser(min: number, max: number) {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  const getUser = async () => {
+    try {
+      let userId: number = randomUser(1, 10);
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users/' + userId.toString(),
+      );
+      const json = await response.json();
+      const userArray: User[] = [json];
+      setUsers(userArray);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
- 
-    const getUser = async () => {
-      try {
-        let userId:number = randomUser(1, 10);
-        const response = await fetch('https://jsonplaceholder.typicode.com/users/'+ userId.toString());
-        const json = await response.json();
-        const userArray:User[] = [json];
-        setUsers(userArray);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
- 
-   
-    useEffect(() => {
-      getUser();
-    }, []);
- 
-    return (
-      <SafeAreaView style={{backgroundColor: 'yellow'}}>
-        {isLoading ? (
-          <ActivityIndicator />
-        ) : (
-          <View>
-              <Text>{users[0].name}</Text>
-              <Text>{users[0].username}</Text>
-              <Text>{users[0].email}</Text>
-              <Text>{users[0].phone}</Text>
-          </View>
-        )}
-      </SafeAreaView>
-    );
-    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    <SafeAreaView>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <View style={styles.container}>
+          <Text style={styles.text}>{users[0].name}</Text>
+          <Text style={styles.text}>{users[0].username}</Text>
+          <Text style={styles.text}>{users[0].email}</Text>
+          <Text style={styles.text}>{users[0].phone}</Text>
+        </View>
+      )}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: 'slateblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#ffffff',
+    fontSize: 20,
+    fontWeight: '300',
+  },
+});
 
 export default ProfileScreen;
